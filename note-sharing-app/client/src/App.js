ed601272e1b63c5a0b3bf37d1a519cbd76f4f2cf
Aditya Choudhary <149503228+AdityaChoudhary01@users.noreply.github.com>
2025-07-15 20:18:06 +0530

    Update App.js

--- note-sharing-app/client/src/App.js
+++ note-sharing-app/client/src/App.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Navbar from './components/layout/Navbar';
-import Footer from './components/layout/Footer'; // Import the new footer
+import Footer from './components/layout/Footer';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import UploadPage from './pages/UploadPage';
 import ViewNotePage from './pages/ViewNotePage';
-import SearchPage from './pages/SearchPage'; // Import the new page
-import PrivateRoute from './utils/PrivateRoute';
+import SearchPage from './pages/SearchPage';
 import AboutPage from './pages/AboutPage';
-import './App.css';
 import ContactPage from './pages/ContactPage';
 import DonatePage from './pages/DonatePage';
+import PrivateRoute from './utils/PrivateRoute';
+import ProfilePage from './pages/ProfilePage'; // --- ADD THIS IMPORT ---
+import './App.css';
 
 function App() {
   return (
@@ -27,11 +28,13 @@ function App() {
               <Route path="/login" element={<LoginPage />} />
               <Route path="/signup" element={<SignupPage />} />
               <Route path="/view/:noteId" element={<ViewNotePage />} />
-              <Route path="/search" element={<SearchPage />} /> {/* --- ADD THIS ROUTE --- */}
-              <Route path="/upload" element={<PrivateRoute><UploadPage /></PrivateRoute>} />
+              <Route path="/search" element={<SearchPage />} />
               <Route path="/contact" element={<ContactPage />} /> 
               <Route path="/about" element={<AboutPage />} />
-               <Route path="/donate" element={<DonatePage />} />
+              <Route path="/donate" element={<DonatePage />} />
+              {/* --- ADD THIS ROUTE --- */}
+              <Route path="/profile" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
+              <Route path="/upload" element={<PrivateRoute><UploadPage /></PrivateRoute>} />
             </Routes>
           </main>
           <Footer />
@@ -40,6 +43,5 @@ function App() {
     </AuthProvider>
   );
 }
-
 
 export default App;
