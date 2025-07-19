196345108a0d6b571fab6a15605272b6d3e05368
Aditya Choudhary <149503228+AdityaChoudhary01@users.noreply.github.com>
2025-07-19 17:29:16 +0530

    Update server.js

--- note-sharing-app/server/server.js
+++ note-sharing-app/server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('./models/Comment');
 require('dotenv').config();
 
 // --- REQUIRE ALL ROUTE FILES ---
