0a0c678eee9bebcf2f69f55b3434f15525823a6d
Aditya Choudhary <149503228+AdityaChoudhary01@users.noreply.github.com>
2025-07-19 17:27:33 +0530

    Update Note.js

--- note-sharing-app/server/models/Note.js
+++ note-sharing-app/server/models/Note.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const reviewSchema = new Schema({
+  user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  rating: { type: Number, required: true, min: 1, max: 5 },
+  comment: { type: String, required: true }
+}, {
+  timestamps: true
+});
 
 const NoteSchema = new Schema({
   title: { type: String, required: true },
@@ -14,6 +21,17 @@ const NoteSchema = new Schema({
   cloudinaryId: { type: String, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   uploadDate: { type: Date, default: Date.now },
+  reviews: [reviewSchema],
+  rating: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0
+  },
 });
 
 module.exports = mongoose.model('Note', NoteSchema);
