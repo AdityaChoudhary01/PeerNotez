e99e18c520d590d19d019d4cda4e0fb7216259bc
Aditya Choudhary <149503228+AdityaChoudhary01@users.noreply.github.com>
2025-10-01 15:20:59 +0530

    Update postcss.config.js

--- note-sharing-app/client/postcss.config.js
+++ note-sharing-app/client/postcss.config.js
@@ -1,8 +1,6 @@
 module.exports = {
   plugins: {
-    // This correctly runs the Tailwind engine with its own config.
-    '@tailwindcss/postcss': { config: './tailwind.config.js' },
-    // Autoprefixer is often necessary to avoid vendor prefix warnings.
-    'autoprefixer': {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
