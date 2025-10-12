0ea23f371772ca25b15e91c8c5834ebcdec189db
Aditya Choudhary <149503228+AdityaChoudhary01@users.noreply.github.com>
2025-10-12 15:20:24 +0530

    Update index.js

--- note-sharing-app/client/src/index.js
+++ note-sharing-app/client/src/index.js
@@ -8,6 +8,16 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-
 );
-serviceWorkerRegistration.register();
+
+// Register service worker with auto-update
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    // Notify user or force reload when a new SW is available
+    if (registration && registration.waiting) {
+      console.log("New version available, reloading...");
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      window.location.reload();
+    }
+  },
+});
